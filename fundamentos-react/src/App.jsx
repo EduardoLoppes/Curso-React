a1837e252e9af47d825624aa150d426775179784
EduardoLoppes <mcfgonzalles@gmail.com>
2023-06-23 11:48:52 -0300

    aula 20 e 21: Resolução do desafio de numeros aleatórios

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import First from './components/basics/First component'
 import ComParametro from './components/basics/ComParametro'
 import Fragmento from './components/basics/Fragmento'
-
+import Aleatorio from "./components/basics/Aleatorio";
 
 export default _ =>
     <div id="app">
+        <Aleatorio min={10} max={60} />
         <h1>Fundamentos React (Arrow _)</h1>
         <Fragmento />
         <ComParametro titulo="Situação do aluno"
