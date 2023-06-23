17d6cabd57c600f0f7b3a5f97710a25289a07396
EduardoLoppes <mcfgonzalles@gmail.com>
2023-06-23 09:41:46 -0300

    aula 17 de React

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import First from './components/basics/First component'
+import WithParameter from './components/basics/WithParameter'
+
+
+ReactDOM.render(
+    <div id="app">
+        <First></First>
+        <WithParameter titulo="Situação do aluno"
+            aluno="Paulo"
+            nota="9.5"
+        />
+    </div>,
+    document.getElementById('root')
+)
