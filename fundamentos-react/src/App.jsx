010d9e83badd9e27843560d078fbfee092e8febf
EduardoLoppes <mcfgonzalles@gmail.com>
2023-06-26 10:09:55 -0300

    aula 23: Utilizando component card para organizar as partes do projeto

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+
+import Card from "./components/layout/Card";
 import First from './components/basics/First component'
 import ComParametro from './components/basics/ComParametro'
 import Fragmento from './components/basics/Fragmento'
@@ -6,11 +9,29 @@ import Aleatorio from "./components/basics/Aleatorio";
 
 export default _ =>
     <div id="app">
-        <Aleatorio min={10} max={60} />
-        <h1>Fundamentos React (Arrow _)</h1>
-        <Fragmento />
-        <ComParametro titulo="Situação do aluno"
-            aluno="Paulo"
-            nota={9.5} />
-        <First />
+        <h1>Fundamentos do React</h1>
+
+
+        <Card titulo="#04 Desafio Aleatório">
+            <Aleatorio min={10} max={60} />
+        </Card>
+
+        <Card titulo="#03 Fragmento">
+            <Fragmento />
+        </Card>
+
+        <Card titulo="#02 Com Parametro">
+            <ComParametro titulo="Situação do aluno"
+                aluno="Paulo"
+                nota={9.5} />
+
+        </Card>
+
+        <Card titulo="#01 Primeiro Componente">
+            <First />
+
+        </Card>
+
+
+
     </div>
--- fundamentos-react/src/components/basics/Aleatorio.jsx
+++ fundamentos-react/src/components/basics/Aleatorio.jsx
@@ -6,10 +6,10 @@ export default props => {
     const aleatorio = parseInt(Math.random() * (max - min)) + min;
     return (
         <div>
-            <h2>Valor Aleatório</h2>
-            <p><strong>Valor mínimo</strong>{min}</p>
+            <h2>Aleatório</h2>
+            <p><strong>mínimo</strong>{min}</p>
             <p><strong>Valor máximo</strong>{max}</p>
-            <p><strong>Valor escolhido: </strong>{aleatorio}</p>
+            <p><strong>escolhido: </strong>{aleatorio}</p>
         </div>
     )
 }
