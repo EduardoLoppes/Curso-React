055b8dc0535ba32953adce43c2674578b1e924e0
EduardoLoppes <mcfgonzalles@gmail.com>
2023-06-28 09:05:57 -0300

    Aula 24: Configurando flex box com cards

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 
-
+import "./App.css";
 import Card from "./components/layout/Card";
-import First from './components/basics/First component'
-import ComParametro from './components/basics/ComParametro'
-import Fragmento from './components/basics/Fragmento'
+import First from "./components/basics/First component";
+import ComParametro from "./components/basics/ComParametro";
+import Fragmento from "./components/basics/Fragmento";
 import Aleatorio from "./components/basics/Aleatorio";
 
 export default _ =>
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos do React</h1>
+        <div className="Cards">
 
+            <Card titulo="#04 Desafio Aleatório">
+                <Aleatorio min={10} max={60} />
+            </Card>
 
-        <Card titulo="#04 Desafio Aleatório">
-            <Aleatorio min={10} max={60} />
-        </Card>
+            <Card titulo="#03 Fragmento">
+                <Fragmento />
+            </Card>
 
-        <Card titulo="#03 Fragmento">
-            <Fragmento />
-        </Card>
+            <Card titulo="#02 Com Parametro">
+                <ComParametro titulo="Situação do aluno"
+                    aluno="Paulo"
+                    nota={9.5} />
 
-        <Card titulo="#02 Com Parametro">
-            <ComParametro titulo="Situação do aluno"
-                aluno="Paulo"
-                nota={9.5} />
+            </Card>
 
-        </Card>
+            <Card titulo="#01 Primeiro Componente">
+                <First />
 
-        <Card titulo="#01 Primeiro Componente">
-            <First />
-
-        </Card>
-
+            </Card>
+        </div>
 
 
     </div>
